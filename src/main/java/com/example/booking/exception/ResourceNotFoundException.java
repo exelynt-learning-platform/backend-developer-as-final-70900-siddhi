3a2dfad5fd6116ffdbe4170c889d2985146a6ee6
Siddhi Arote <siddhiarote5@gmail.com>
2026-09-02 09:35:03 +0530

    update final booking system code

--- src/main/java/com/example/booking/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/booking/exception/ResourceNotFoundException.java
@@ -3,9 +3,9 @@ package com.example.booking.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
-public class DuplicateResourceException extends RuntimeException {
-    public DuplicateResourceException(String message) {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
